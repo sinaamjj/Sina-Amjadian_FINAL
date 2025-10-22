0ad6f879fae9611bfb7ffed4d6b852b200f87a97
Sina Amjadian <sina.amjjj@gmail.com>
2025-10-22 23:22:15 +0330

    first commit

---
+++ Front/components/auth/Login.js
@@ -0,0 +1,92 @@
+import styles from "./Login.module.css";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useEffect, useRef } from "react";
+
+function Login({ onClose, onSuccess }) {
+  const modalRef = useRef(null);
+
+  const validationSchema = Yup.object({
+    phone: Yup.string()
+      .required("شماره موبایل الزامی است")
+      .test(
+        "is-valid-phone",
+        "شماره موبایل معتبر نیست",
+        (value) => !value || /^09\d{9}$/.test(value)
+      ),
+  });
+
+  const formik = useFormik({
+    initialValues: { phone: "" },
+    validationSchema,
+    onSubmit: async (values) => {
+      try {
+        const res = await axios.post("http://localhost:6500/auth/send-otp", {
+          mobile: values.phone,
+        });
+
+        console.log("✅:", res.data);
+        toast.success("کد تایید با موفقیت ارسال شد ✅");
+
+        if (typeof onSuccess === "function") {
+          onSuccess(values.phone);
+        }
+      } catch (error) {
+        console.error("Error in send-otp:", error);
+        toast.error("❌ ارسال کد با خطا مواجه شد!");
+      }
+    },
+  });
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [onClose]);
+
+  return (
+    <div className={styles.backDrop}>
+      <form
+        ref={modalRef}
+        className={styles.container}
+        onSubmit={formik.handleSubmit}
+      >
+        <button className={styles.closeBtn} type="button" onClick={onClose}>
+          ×
+        </button>
+
+        <h1>ورود به تورینو</h1>
+
+        <div className={styles.inputGroup}>
+          <p>شماره موبایل خود را وارد کنید</p>
+          <input
+            type="text"
+            name="phone"
+            value={formik.values.phone}
+            onChange={formik.handleChange}
+            placeholder="2032***0912"
+          />
+        </div>
+
+        {formik.touched.phone && formik.errors.phone && (
+          <div className={styles.errorText}>{formik.errors.phone}</div>
+        )}
+
+        <button className={styles.btn} type="submit">
+          ارسال کد تایید
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
